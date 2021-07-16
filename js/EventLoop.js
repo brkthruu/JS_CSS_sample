68c110cf87a95a853f45d629c9e3b538fb3f4ed6
hysimok <jeonghy95@gmail.com>
2021-07-16 12:53:43 +0900

    JS asynchronous process

---
+++ js/EventLoop.js
@@ -0,0 +1,17 @@
+console.log('Life');
+
+setTimeout(function () {
+	console.log('42');
+}, 3000);
+
+console.log('is');
+
+/*
+console.log('Life');
+
+setTimeout(function () {
+	console.log('42');
+}, 0);
+
+console.log('is');
+*/
